7b4df53cbeb3e40f4716b2954c195f58a485afc9
Jay Tare <jaytare414@gmail.com>
2026-02-19 16:02:14 +0530

    fix vite config for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Digital-Rationcard-System/',  // 👈 added this
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-}
-)
+})
